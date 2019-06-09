56c3b1e1a11687f7791a2c4244c3f84034abb5d2
Aggrey <39681748+Emmanuel-Aggrey@users.noreply.github.com>
2019-06-09 23:04:47 +0000

    Update secret.py

--- esterntour/secret.py
+++ esterntour/secret.py
@@ -1,6 +1,3 @@
 
-EMAIL = 'koforiduaecotourismghana@gmail.com'
-PASSWORD = 'kwamelove65'
-SECRET_KEY = 'jf$ac236s)uv)6xa&o0#@wmt+37zvlwf=atbi)%et^7=%3#$bb'
-
-
+EMAIL = 'GMAIL ADDRESS'
+PASSWORD = 'GMAIL PASSWORD'
